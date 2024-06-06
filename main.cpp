32b0eed86b796b7fd13e0b7af1d21f892fde4173
RaKalik963 <andrejmahot646@gmail.com>
2024-06-06 20:01:27 +0300

    1221

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@ using namespace std;
 
 int main()
 {
-
+	srand(0);
+	const int size = 100;
+	int arr[size];
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[size] = srand() % 100;
+	}
 	return 0;
 	system("pause");
 }
