461595d104ed6e48bbf1867d41902361b2df4a1c
RaKalik963 <andrejmahot646@gmail.com>
2024-06-06 19:18:47 +0300

    12332

--- main.cpp
+++ main.cpp
@@ -2,10 +2,20 @@
 #include <ctime>
 using namespace std;
 
-
+int row(int N)
+{
+	for (size_t i = 0; i < N; i++)
+	{
+		cout << "*" << " ";
+	}
+	cout << endl;
+}
 
 int main()
 {
+	int N;
+	cout << "enter the count of stars";
+	cin >> N;
 
 	return 0;
 	system("pause");
