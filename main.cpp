45731db7d74593a66f6d2165db2cb0fbcbea2029
RaKalik963 <andrejmahot646@gmail.com>
2024-06-06 19:54:39 +0300

    21321323213

--- main.cpp
+++ main.cpp
@@ -2,11 +2,33 @@
 #include <ctime>
 using namespace std;
 
-
+template <typename T>
+T diapasone_from_a_to_b(T a, T b)
+{
+	
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a == b)
+	{
+		return 1;
+	}
+	else
+	{
+		a += b;
+	}
+}
 
 int main()
 {
+	int a, b;
+	cout << "enter a start of diapasone";
+	cin >> a;
+	cout << "enter the end of diapasone";
+	cin >> b;
 
+	cout <<diapasone_from_a_to_b<int>(a, b) << endl;
 	return 0;
 	system("pause");
 }
