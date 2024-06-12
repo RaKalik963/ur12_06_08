2c30e2819dedcfd4a3a710ac8bd0d954cece6cc5
RaKalik963 <andrejmahot646@gmail.com>
2024-06-12 16:03:43 +0300

    dz-ex2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    else {
+        return gcd(b, a % b);
+    }
+}
 
+int main() {
+    int a, b;
+    cout << "enter num1 ";
+    cin >> a;
+    cout << "enter num2 ";
+    cin >> b;
 
-int main()
-{
+    cout << "GCD of " << a << " and " << b << " = " << gcd(a, b) << endl;
 
-	return 0;
-	system("pause");
+    return 0;
 }
